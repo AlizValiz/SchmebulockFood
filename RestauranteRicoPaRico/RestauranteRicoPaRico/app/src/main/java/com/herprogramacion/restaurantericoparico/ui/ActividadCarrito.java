679034c282c3ee04f40dfab563b81c0c10e0279d
Daniel Alejandro Dominguez Meza <magnotec28@gmail.com>
2016-05-24 01:02:42 -0500

    Cambios a carro y a JSON Parser

--- RestauranteRicoPaRico/RestauranteRicoPaRico/app/src/main/java/com/herprogramacion/restaurantericoparico/ui/ActividadCarrito.java
+++ RestauranteRicoPaRico/RestauranteRicoPaRico/app/src/main/java/com/herprogramacion/restaurantericoparico/ui/ActividadCarrito.java
@@ -2,6 +2,7 @@ package com.herprogramacion.restaurantericoparico.ui;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
@@ -19,7 +20,10 @@ import android.widget.ListView;
 import com.herprogramacion.restaurantericoparico.R;
 import com.herprogramacion.restaurantericoparico.modelo.AdaptadorProducto;
 import com.herprogramacion.restaurantericoparico.modelo.Comida;
+import com.herprogramacion.restaurantericoparico.modelo.JSONParser;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -29,7 +33,7 @@ public class ActividadCarrito extends Activity {
 
     private List<Comida> mCartList;
     private AdaptadorProducto mProductAdapter;
-
+    private ArrayList<String> data = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,9 @@ public class ActividadCarrito extends Activity {
         // Make sure to clear the selections
         for(int i=0; i<mCartList.size(); i++) {
             mCartList.get(i).selected = false;
+            data.add(mCartList.get(i).getNombre());//0 - 2 - 4 - 6
+            data.add(Float.toString(mCartList.get(i).getPrecio()));//1 - 3 - 5 - 7
+
         }
 
         // Create the list
@@ -77,14 +84,31 @@ public class ActividadCarrito extends Activity {
             }
         });
 
-
+        Button sendOrderButton = (Button) findViewById(R.id.sendData);
+        sendOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AsyncTask<String, Void, String> execute = new JSONParser(getApplicationContext()).
+                        execute("1", new Date().toString()
+                                , data.get(0)//plat1
+                                , data.get(1)//plat1price
+                                , data.get(2)//plat2
+                                , data.get(3)//plat2price
+                                , data.get(4)//plat3
+                                , data.get(5)//plat3price
+                                , data.get(6)//bebida
+                                , data.get(7)//bebidaprice
+                        );
+            }
+        });
 
     }
 
 //    private static final String INDICE_SECCION
 //            = "com.restaurantericoparico.FragmentoCategoriasTab.extra.INDICE_SECCION";
 
-
+//    AsyncTask<String, Void, String> execute = new JSONParser(getActivity()).
+//            execute("1", new Date().toString(), "Salmon", "120.50", "Pan", "2.80", "Ensalada", "60.99 ", "Refresco", "10.50");
 //    private RecyclerView reciclador;
 //    private GridLayoutManager layoutManager;
 //    private AdaptadorCategorias adaptador;
